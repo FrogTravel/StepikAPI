860207b729b5b4380cd29d76ca749e5dfaea42bb
FrogTravel <e.levchenko@innopolis.ru>
2018-05-05 02:08:50 +0300

    Picture added (fucking picasso)

--- app/src/main/java/nekono/inno/stepikapi/CourseResultAdapter.kt
+++ app/src/main/java/nekono/inno/stepikapi/CourseResultAdapter.kt
@@ -1,15 +1,11 @@
 package nekono.inno.stepikapi
 
-import android.support.v7.content.res.AppCompatResources.getDrawable
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.GlideBuilder
-import com.bumptech.glide.load.model.GlideUrl
 import com.squareup.picasso.Picasso
 
 class CourseResultAdapter(val presenter : SearchCourse.Presenter) : RecyclerView.Adapter<CourseResultAdapter.CourseViewHolder>() {
